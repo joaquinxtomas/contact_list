07884e041c10022d1282f74f63b50a0f305a201d
joaquinxtomas <jtppalacios@gmail.com>
2023-08-13 14:00:01 -0300

    with_functions_module

---
+++ functions.py
@@ -0,0 +1,23 @@
+import re
+
+def validar_numero():
+    patron = r'11\d{8}'
+    
+    while True:
+        numero = input("Ingrese el numero: ")
+        
+        if re.match(patron,numero):
+            return numero
+        else:
+            print("Numero inválido!\n")
+            
+def validar_email():
+    patron = r'[a-zA-Z0-9]{1,}@[a-zA-Z]{1,}\.[a-zA-Z]'
+    
+    while True:
+        email = input("Ingrese el email: ")
+        
+        if re.match(patron, email):
+            return email
+        else:
+            print("Esto es incorrecto")
--- registro.py
+++ registro.py
@@ -3,7 +3,7 @@ Crear clase Contacto con atributos nombre, numero y correo electronico (validar 
 Agregar metodo para agregar un contacto 
 Agregar metodo que busque el contacto por nombre o numero
 """
-import re
+import functions as validations
 
 class Contacto:
     def __init__(self, nombre,telefono, email):
@@ -21,28 +21,6 @@ class Contacto:
                 self.nombre,self.telefono,self.email = tupla
                 return True
         return False
-
-def validar_numero():
-    patron = r'11\d{8}'
-    
-    while True:
-        numero = input("Ingrese el numero: ")
-        
-        if re.match(patron,numero):
-            return numero
-        else:
-            print("Numero inválido!\n")
-            
-def validar_email():
-    patron = r'[a-zA-Z0-9]{1,}@[a-zA-Z]{1,}\.[a-zA-Z]'
-    
-    while True:
-        email = input("Ingrese el email: ")
-        
-        if re.match(patron, email):
-            return email
-        else:
-            print("Esto es incorrecto")
             
 contactos = []
 contacto_nuevo = (Contacto("","",""))
@@ -57,8 +35,8 @@ while True:
     else:
         print("\n----------------\nNo se ha encontrado el contacto. Vamos a agregarlo.\n")
         nombre = input("Ingrese su nombre: ")
-        numero = validar_numero()
-        email = validar_email()
+        numero = validations.validar_numero()
+        email = validations.validar_email()
         contacto_nuevo = Contacto(nombre,numero,email)
         contacto_nuevo.agregar_contacto(contactos)
         
